58f8809c16f3d3916cad798184a622d620924039
noeerolli <noeliarolli82@gmail.com>
2022-12-11 12:01:07 -0300

    estilos cart

--- src/components/main/pages/ItemDetails.jsx
+++ src/components/main/pages/ItemDetails.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { CartBox } from "../../cart/CartBox";
 //import { useFetch } from "../../../hooks/useFetch";
@@ -26,54 +25,12 @@ export const ItemDetails = () =>{
     }, [id])
 
     return(<>
-{/*
-        <div>
-    {console.log(id)}
-        <h1 className="title" style={{marginTop:"2em", marginBottom: "2em"}}>Detalles del producto</h1>
-
-        <Container className="d-flex justify-content-center ">
-
-         
-
-         <Card className="text-center" style={{ width: '60rem', display:"flex", flexDirection:"row" }}>
-
-                <div style={{width:"30%"}}>
-                    <Card.Header>{item.name}</Card.Header>
-                    <div style={{display:"flex"}}> <img className="card-img"  src={item.image} alt={item.name}/> </div>
-                </div>
-                
-                <div className="card-body" style={{width:"70%", alignItems:"center", padding:"0" }} >
-                  
-                    <Card.Header>Precio: {item.price}</Card.Header>
-                    
-                    <div style={{height:"80%"}}>
-
-                        <div style={{textAlign: "center"}}>
-                            <Card.Text>
-                            {item.description}
-                            </Card.Text>
-                            <Card.Text>
-                            {item.details}
-                            </Card.Text>
-                        </div>
-                        
-                        <div style={{display:"flex", flexWrap:"wrap", position:"absolute", marginBottom:"1em"}}>
-                            <CartBox products={item}></CartBox >
-                        </div>
-                    
-                    </div>
-                   
-                </div>   
-                
-            </Card>
-    </Container>*/}
 
             <h1 className="title" style={{marginTop:"2em", marginBottom: "2em"}}>Detalles del producto</h1>
 
-            
-            
                 
                 <div className="card" style={{display:"flex", flexDirection:"row", margin:"4em"}} >
+                    
                     <div className="imgContainer" style={{display:"flex", flexDirection:"column"}}>
                         <img className="card-img" src={item.image} alt={item.name} />
                         <p>Precio: {item.price}</p>
@@ -95,6 +52,7 @@ export const ItemDetails = () =>{
                         <CartBox products={item}></CartBox >
                         
                     </div>
+                    
                 </div>
 
 
