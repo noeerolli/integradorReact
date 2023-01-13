0218f3e1836021cfc837ce6ac679efc9e4cb84c0
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-01-13 15:04:39 -0300

    Agregado Buscador

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,19 +1,33 @@
+import { useState } from "react"
 import { Button, Form } from "react-bootstrap"
+
 
 
 export const SearchBar = () => {
 
+    const [searchParam, setSearchParam] = useState("")
 
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setSearchParam(e.target.value)
+
+
+    }
+   
 
     return (
-        <Form className="d-flex p-2">
+        <Form className="d-flex p-2" >
             <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={searchParam}
+                onChange={handleChange}
+
             />
-            <Button variant="outline-warning">Buscar</Button>
+            <Button variant="outline-warning" href={`/SearchPage/${searchParam}`}>Buscar</Button>
         </Form>
     )
 }
